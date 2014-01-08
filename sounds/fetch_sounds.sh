3f4786a024fd1ad98a13631b1e2e7561533e0215
Christy O'Reilly <christy@oreills.co.uk>
2014-01-08 19:24:01 +0000

    Don't clobber

--- sounds/fetch_sounds.sh
+++ sounds/fetch_sounds.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-wget http://www.freespecialeffects.co.uk/soundfx/household/camera.wav
-wget http://www.freespecialeffects.co.uk/soundfx/cartoon/popout.wav
-wget http://www.freespecialeffects.co.uk/soundfx/computers/computerbeep2.wav
-wget http://www.freespecialeffects.co.uk/soundfx/computers/computerbeep3.wav
-wget http://www.freespecialeffects.co.uk/soundfx/computers/poweron.wav
-wget http://www.freespecialeffects.co.uk/soundfx/computers/poweroff.wav
-wget http://www.freespecialeffects.co.uk/soundfx/computers/shutdowninprocess.wav
-wget http://www.freespecialeffects.co.uk/soundfx/cartoon/whizfst2.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/household/camera.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/cartoon/popout.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/computers/computerbeep2.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/computers/computerbeep3.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/computers/poweron.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/computers/poweroff.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/computers/shutdowninprocess.wav
+wget --no-clobber http://www.freespecialeffects.co.uk/soundfx/cartoon/whizfst2.wav
