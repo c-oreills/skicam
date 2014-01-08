8ecc353c6386912bbca57390cae8435d86b81805
Christy O'Reilly <christy@oreills.co.uk>
2014-01-08 20:46:46 +0000

    Add shutdown sound for webserver

--- web/utils.py
+++ web/utils.py
@@ -6,6 +6,7 @@ def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
+    subprocess.Popen(['aplay', '../sounds/poweroff.wav'])
     func()
 
 def get_disk_free():
