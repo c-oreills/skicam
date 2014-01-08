7700a5124fa807b7147622e5c66860054fc06462
Christy O'Reilly <christy@oreills.co.uk>
2014-01-08 19:11:56 +0000

    Add sounds to menu items

--- menus.py
+++ menus.py
@@ -4,14 +4,22 @@ import subprocess
 from capture import pic, vid
 
 
+def play_sound(name):
+    subprocess.Popen(['aplay', 'sounds/{n}.wav'.format(n=name)])
+
+def capture_pic():
+    pic()
+    play_sound('camera')
+
 def start_video():
     vid_p = vid()
+    play_sound('whizfst2')
 
     def stop_video():
         vid_p.terminate()
         switch_menu(CAPTURE_MENU)
 
-    switch_menu({'default': stop_video})
+    switch_menu({'name': 'stop_vid', 'default': stop_video})
 
 flask_p = None
 
@@ -25,9 +33,12 @@ def start_flask():
             ['/home/pi/.virtualenvs/skicam/bin/python',
                 '/home/pi/skicam/web/skiweb.py', str(getpid())],
             cwd='/home/pi/skicam/web/')
+    play_sound('poweron')
 
 CAPTURE_MENU = {
-    1: pic,
+    'name': 'capture',
+    'sound': 'computerbeep2',
+    1: capture_pic,
     2: start_video,
     3: lambda: switch_menu(MUSIC_MENU),
     4: start_flask,
@@ -42,6 +53,8 @@ def play_pause():
     pass
 
 MUSIC_MENU = {
+    'name': 'music',
+    'sound': 'computerbeep3',
     1: skip_track,
     2: play_pause,
     3: lambda: switch_menu(CAPTURE_MENU),
@@ -54,6 +67,9 @@ current_menu = CAPTURE_MENU
 def switch_menu(menu):
     global current_menu
     current_menu = menu
+    sound = menu.get('sound')
+    if sound:
+        play_sound(sound)
 
 def exec_menu_option(n, eager=False):
     # If eager_first is set, only exec option 1 if eager is True, and any other
---
+++ sounds/fetch_sounds.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget http://www.freespecialeffects.co.uk/soundfx/household/camera.wav
+wget http://www.freespecialeffects.co.uk/soundfx/cartoon/popout.wav
+wget http://www.freespecialeffects.co.uk/soundfx/computers/computerbeep2.wav
+wget http://www.freespecialeffects.co.uk/soundfx/computers/computerbeep3.wav
+wget http://www.freespecialeffects.co.uk/soundfx/computers/poweron.wav
+wget http://www.freespecialeffects.co.uk/soundfx/computers/poweroff.wav
+wget http://www.freespecialeffects.co.uk/soundfx/computers/shutdowninprocess.wav
+wget http://www.freespecialeffects.co.uk/soundfx/cartoon/whizfst2.wav
